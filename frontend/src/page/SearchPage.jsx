eb5bb5f180948cd1e361fe23c27ea9ef95654df4
dineshkarde103 <dineshkarde103@gmail.com>
2023-04-26 10:21:35 +0530

    create search bar

--- frontend/src/page/SearchPage.jsx
+++ frontend/src/page/SearchPage.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { Box, Input, Text } from '@chakra-ui/react'
+import React, { useState } from 'react'
 
 const SearchPage = () => {
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleChange = (e) =>{
+    setSearchInput(e.target.value);
+  }
+
   return (
-    <div>SearchPage</div>
+    <Box width={"80%"} margin={"20px auto"}>
+      <Input placeholder='Search here' onChange={handleChange}/>
+      <Text>{searchInput}</Text>
+    </Box>
   )
 }
 
