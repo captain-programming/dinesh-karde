f36f7411a7af0b2c60b2e29d3d25d10ba2658dba
dineshkarde103 <dineshkarde103@gmail.com>
2023-06-18 21:27:41 +0530

    update

--- frontend/src/store/actions.js
+++ frontend/src/store/actions.js
@@ -4,7 +4,7 @@ import axios from "axios";
 export const SearchText = (text) => async(dispatch) => {
   dispatch({type: LOADING});
   try{
-    let {data} = await axios.get(`http://localhost:8080/ads/search?search=${text}`);
+    let {data} = await axios.get(`https://clever-capris-jay.cyclic.app/ads/search?search=${text}`);
     dispatch({type: SEARCH_ADS, payload: data})
   }catch(err){
     dispatch({type: ERROR, payload: err.message})
